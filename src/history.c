00b9dfa24528f1298f473ae7c96f6083af6ef178
ljlabayen <ljlabayen2@miners.utep.edu>
2020-06-23 05:42:17 -0600

    added get_history print_history and free_history

--- src/history.c
+++ src/history.c
@@ -26,3 +26,38 @@ void add_history(List *list, char *str) {
     temp->next->next = NULL;
   }
 }
+
+char* get_history(List *list, int id) {
+  if (list->root == NULL) {
+    return "List is empty";
+  } else {
+    Item* temp = list->root;
+    while (temp != NULL) {
+      if (temp->id == id) {
+	return temp->str;
+      }
+      temp = temp->next;
+    }
+    return "Item ID not found";
+  }
+}
+
+void print_history(List *list) {
+  Item* temp = (list->root);
+  while (temp != NULL) {
+    printf("Item [%d]: %s \n", temp->id, temp->str);
+    temp = temp->next;
+  }
+}
+
+void free_history(List *list) {
+  Item* temp;
+  Item* temp2;
+  temp2 = list->root;
+  while (temp2 != NULL) {
+    temp =  temp2;
+    temp2 = temp2->next;
+    free(temp);
+  }
+  free(list);
+}
